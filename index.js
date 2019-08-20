e6ca678db190bda551d8f0e0df3611ca4550b03b
BradleyTim <timbradley94@gmail.com>
2019-08-20 21:23:03 +0300

    first commit && server set-up

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
