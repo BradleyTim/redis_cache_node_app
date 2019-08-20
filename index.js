daf377cb2b489bfeaa32816758dfb0375255ed77
BradleyTim <timbradley94@gmail.com>
2019-08-20 21:40:52 +0300

    add getRepos function logic to fetch data from api

--- index.js
+++ index.js
@@ -9,4 +9,28 @@ const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
+async function getRepos(req, res, next) {
+  try {
+    console.log("FETCHING DATA");
+
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    //set data to redis cache
+    client.setex(username, 3600, repos);
+
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+}
+
+app.get("/repos/:username", getRepos);
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
